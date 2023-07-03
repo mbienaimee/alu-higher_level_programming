73bd2987890316c9ac09f7c4d6be0d94ea553672
root <m.bienaimee@alustudent.com>
2023-07-03 11:17:43 -0700

    loop

--- python-if_else_loops_functions/0-positive_or_negative.py
+++ python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
 import random
-number = random.randint(-10000, 10000)
-if number == 0:
-    print(f'{number} is zero')
-elif number > 0:
-    print(f'{number} is positive')
-else:
-    print(f'{number} is negative')
+number = random.randint(-10000, 1000)
+def pnive(x):
+
+    if x > 0:
+        print(f"{x} is positive")
+    elif x < 0:
+        print(f"{x} is negative")
+    else:
+        print(f"{x} is zero")
+
+
+pnive(number)
