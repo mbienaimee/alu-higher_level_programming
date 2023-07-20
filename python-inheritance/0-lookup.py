273faf936ba551e8970bd273cf4e8a4eed0e64d8
root <m.bienaimee@alustudent.com>
2023-07-20 14:09:33 -0700

    python inhertance

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# return the list of available attributes and methds of an object
+"""
+    define a function ' lookup'
+"""
+
+
+def lookup(obj):
+    """
+        Return:list object
+    """
+    return (dir(obj))
