a1f56cc864ba2b461dd16d88d4188f6216c32abf
root <m.bienaimee@alustudent.com>
2023-07-30 01:51:48 -0700

    sql

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,3 +1,6 @@
--- create use if not there 
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost'
+-- create user
+CREATE USER IF NOT EXISTS user_0d_1@localhost
+IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL
+ON *.*
+TO user_0d_1@localhost;
