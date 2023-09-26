da9a245add46ec78ed0be6ce39efe21730b83b59
mbienaimee <m.bienaimee@alustudent.com>
2023-09-26 08:54:40 -0700

    first

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,12 +1,10 @@
-#!/usr/bin/nodec
-const x=12;
-if(x===null){
-	console.log('No argument');
-}
-else if( x!==null){
-	console.log('Argument found')
+#!/usr/bin/node
+const args = process.argv.slice(2);
 
-}
-else{
-     console.log(Arguments found);
+if (args.length === 0) {
+  console.log('No argument');
+} else if (args.length === 1) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
